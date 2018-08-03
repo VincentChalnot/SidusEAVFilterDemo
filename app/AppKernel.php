92aad82957a144392ebbb9c2e045eba23908f318
Vincent Chalnot <vchalnot@clever-age.com>
2018-08-03 18:41:46 +0200

    Updating readme and template to add pager

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
             new Sidus\BaseBundle\SidusBaseBundle(),
             new Sidus\FilterBundle\SidusFilterBundle(),
             new AppBundle\AppBundle(),
